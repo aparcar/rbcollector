98324635f75924ed3217f589818a36418d8a6e3b
Paul Spooren <mail@aparcar.org>
2020-10-25 16:56:10 -1000

    results/web: allow arbitrary result paths
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- rbcollector/results/web.py
+++ rbcollector/results/web.py
@@ -13,21 +13,24 @@ def get_rbvfs(config, timestamp):
     print("Using generic web")
     rbvfs = []
 
-    for suite in config["web"]["suites"]:
-        for target in config["web"]["targets"]:
-            target_url = f"{config['web']['base_url']}/{suite}/{target}"
-            rbvf = requests.get(f"{target_url}/rbvf.json").json()
+    for suite in config["web"].get("suites", ["unset-suite"]):
+        for component in config["web"].get("components", ["unset-component"]):
+            for target in config["web"].get("targets", ["unset-target"]):
+                target_url = config["web"]["base_url"].format(
+                    suite=suite, target=target, component=component
+                )
+                rbvf = requests.get(f"{target_url}/rbvf.json").json()
 
-            if "timestamp" in rbvf:
-                if (
-                    datetime.fromisoformat(
-                        rbvf["timestamp"].replace("Z", "+00:00")
-                    ).replace(tzinfo=None)
-                    < timestamp
-                ):
-                    continue
+                if "timestamp" in rbvf:
+                    if (
+                        datetime.fromisoformat(
+                            rbvf["timestamp"].replace("Z", "+00:00")
+                        ).replace(tzinfo=None)
+                        < timestamp
+                    ):
+                        continue
 
-            rbvf["storage_uri"] = target_url
-            rbvfs.append(rbvf)
+                rbvf["storage_uri"] = target_url
+                rbvfs.append(rbvf)
 
     return rbvfs
