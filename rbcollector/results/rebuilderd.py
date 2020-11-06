6b9cc5d419dc36690b5da58c69c97fd70fc5f4db
Paul Spooren <mail@aparcar.org>
2020-11-05 16:47:56 -1000

    results,rebuilderd: use requests and fix config
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- rbcollector/results/rebuilderd.py
+++ rbcollector/results/rebuilderd.py
@@ -1,13 +1,22 @@
-import json
-from urllib.request import urlopen
+from json.decoder import JSONDecodeError
+import logging
+import requests
+
+logger = logging.getLogger(__name__)
 
 
 def get_rbvfs(config: dict, timestamp):
-    print("Using Rebuilderd")
-    host, distro = config["uri"].split("#")
-    results = json.loads(
-        urlopen(f"{host}/api/v0/pkgs/list?distro={distro}").read().decode()
-    )
+    logging.info("Using Rebuilderd")
+    host = config["rebuilderd"]["host"]
+    distro = config["rebuilderd"]["distro"]
+
+    try:
+        rbvf_url = f"{host}/api/v0/pkgs/list?distro={distro}"
+        logger.info(f"Downloading {rbvf_url}")
+        results = requests.get(rbvf_url).json()
+    except JSONDecodeError:
+        logger.warning(f"Failed to download rbvf.json")
+        return []
 
     rbvf = {
         "origin_uri": "",
